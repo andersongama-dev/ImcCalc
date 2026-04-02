559c47397f9980fe52272cc8838b24b5874ea165
andersongama-dev <andersongamasilva5@gmail.com>
2026-04-01 23:04:19 -0300

    feat: Add user input handling and UI improvements
    - Capture selected gender, height, weight, and age
    - Make UI functional with real-time updates
    - Adjust button sizes for plus/minus controls
    - Configure primary and secondary theme colors

--- app/src/main/java/nyz/imccalc/MainActivity.kt
+++ app/src/main/java/nyz/imccalc/MainActivity.kt
@@ -1,8 +1,12 @@
 package nyz.imccalc
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -16,5 +20,88 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Gender
+        val buttonMale = findViewById<Button>(R.id.buttonMale)
+        val buttonFemale = findViewById<Button>(R.id.buttonFemale)
+
+        var genderChoice = "" // Gender
+
+        // Height
+        val heightUserValue = findViewById<SeekBar>(R.id.heightUserValue) // Height seekBar
+        val textViewHeightValue = findViewById<TextView>(R.id.textViewHeightValue) // Height textView
+        var valueHeight = 0 // Height Value
+
+        // Weight
+        val buttonLessWeight = findViewById<Button>(R.id.buttonLessWeight) // Weight button less
+        val buttonPlusWeight = findViewById<Button>(R.id.buttonPlusWeight) // Weight button plus
+        val textViewWeight = findViewById<TextView>(R.id.textViewWeight) // Weight textView
+        var valueWeight = 0 // Weight value
+
+        //Age
+        val buttonLessAge = findViewById<Button>(R.id.buttonLessAge) // Age button less
+        val buttonPlusAge = findViewById<Button>(R.id.buttonPlusAge) // Age button plus
+        val textViewAge = findViewById<TextView>(R.id.textViewAge) // Age textView
+        var valueAge = 0 // Age value
+
+        buttonLessAge.setOnClickListener {
+            if(valueAge != 0) {
+                valueAge -= 1
+                textViewAge.text = valueAge.toString()
+            }
+        }
+
+        buttonPlusAge.setOnClickListener {
+            valueAge += 1
+            if(valueAge > 122) {
+                valueAge = 122
+            }
+            textViewAge.text = valueAge.toString()
+        }
+
+
+        buttonLessWeight.setOnClickListener {
+            if(valueWeight != 0) {
+                valueWeight -= 1
+                textViewWeight.text = valueWeight.toString()
+            }
+        }
+
+        buttonPlusWeight.setOnClickListener {
+            valueWeight += 1
+            if(valueWeight > 635) {
+                valueWeight = 635
+            }
+            textViewWeight.text = valueWeight.toString()
+        }
+
+
+        heightUserValue.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                textViewHeightValue.text = progress.toString()
+                valueHeight = progress
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
+
+        buttonMale.setOnClickListener {
+            buttonMale.backgroundTintList = ContextCompat.getColorStateList(this, R.color.primary)
+            buttonFemale.backgroundTintList = ContextCompat.getColorStateList(this, R.color.secondary)
+            genderChoice = "male"
+            findViewById<TextView>(R.id.resultTextView).text = genderChoice
+        }
+
+        buttonFemale.setOnClickListener {
+            buttonFemale.backgroundTintList = ContextCompat.getColorStateList(this, R.color.primary)
+            buttonMale.backgroundTintList = ContextCompat.getColorStateList(this, R.color.secondary)
+            genderChoice = valueHeight.toString()
+            findViewById<TextView>(R.id.resultTextView).text = genderChoice
+        }
+
     }
 }
